71e4f269c7f14febe82d0e050fb997941b48ed50
mesuchan <seminanele@gmail.com>
2019-03-30 16:36:01 +0300

    Help me.

---
+++ CinnabunsFinal/DTO/PageResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinnabunsFinal.DTO
+{
+    public class PageResult<T>
+    {
+        public int TotalCount { get; set; }
+        public List<T> Data { get; set; }
+    }
+}
