abe2e05745a53d08efc0ebf37840ffd98fe23f9b
buglight <buglight@kistriver.com>
2019-03-30 18:45:47 +0300

    Added auth services. Fixed pipeline.

--- CinnabunsFinal/Startup.cs
+++ CinnabunsFinal/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using VueCliMiddleware;
 
 namespace CinnabunsFinal
@@ -13,12 +15,14 @@ namespace CinnabunsFinal
     public class Startup
     {
         public IConfiguration Configuration { get; }
+        public IHostingEnvironment Environment { get; }
 
-        public Startup()
+        public Startup(IHostingEnvironment environment)
         {
             var builder = new ConfigurationBuilder()
                 .AddEnvironmentVariables();
             Configuration = builder.Build();
+            Environment = environment;
         }
 
         public void ConfigureServices(IServiceCollection services)
@@ -40,6 +44,40 @@ namespace CinnabunsFinal
 
             services.AddIdentity<User, IdentityRole<int>>()
                 .AddEntityFrameworkStores<AppContext>();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                if (Environment.IsDevelopment())
+                {
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequiredLength = 1;
+                    options.Password.RequiredUniqueChars = 1;
+                }
+            });
+
+            var authScheme = "JwtBearer";
+            var signingKey = Configuration["SECRETKEY"];
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = authScheme;
+                options.DefaultChallengeScheme = authScheme;
+            })
+            .AddJwtBearer(authScheme, options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    ValidateAudience = false,
+                    ValidateIssuer = false,
+                    ValidateLifetime = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signingKey))
+                };
+            });
+
+            services.AddAuthorization();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -57,16 +95,18 @@ namespace CinnabunsFinal
                     .AllowCredentials();
             });
 
+            app.UseAuthentication();
+
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
 
+            app.UseMvc();
             app.UseSpa(configuration =>
             {
                 configuration.Options.SourcePath = "ClientApp";
                 if (env.IsDevelopment())
                     configuration.UseVueCli("serve", 3000);
             });
-            app.UseMvc();
         }
     }
 }
