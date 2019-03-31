a81edde1be0aeb03381f420d81c81cf6942bb3d2
mesuchan <seminanele@gmail.com>
2019-03-31 05:23:47 +0300

    Model Task was updated.

--- CinnabunsFinal/Migrations/20190331022001_TaskCompleted.Designer.cs
+++ CinnabunsFinal/Migrations/20190331022001_TaskCompleted.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using CinnabunsFinal;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace CinnabunsFinal.Migrations
 {
     [DbContext(typeof(AppContext))]
-    partial class AppContextModelSnapshot : ModelSnapshot
+    [Migration("20190331022001_TaskCompleted")]
+    partial class TaskCompleted
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
